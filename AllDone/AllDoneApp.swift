63428ef7f6fbe77880c4fc4953ee001e5216d361
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-11-04 17:29:31 +0600

    Initial Commit

---
+++ AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by Shishir_Mac on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllDone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllDone
+//
+//  Created by Shishir_Mac on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
