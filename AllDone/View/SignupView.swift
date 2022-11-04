15ad1c37d4889345d96e57ca14a4cbac9dcf6851
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-11-05 00:57:12 +0600

    UserTextField

--- AllDone/View/SignupView.swift
+++ AllDone/View/SignupView.swift
@@ -8,10 +8,56 @@
 import SwiftUI
 
 struct SignupView: View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var email = ""
+    @State var password = ""
+    @State var confirmPassword = ""
+    
+    @Environment(\.presentationMode) var mode
+    
     var body: some View {
         ZStack {
             BackgroundGradientView()
-        }
+            
+            VStack {
+                LogoView()
+                    .padding(.bottom, 25)
+                
+                VStack(spacing: 20) {
+                    UserTextField(text: $firstName, placeholder: "First Name")
+                    UserTextField(text: $lastName, placeholder: "Last Name")
+                    EmailTextField(text: $email)
+                    PasswordSecureField(text: $password, placeholder: "Password")
+                    PasswordSecureField(text: $confirmPassword, placeholder: "Confirm Password")
+                } // VStack
+                .padding(.horizontal, 32)
+                
+                Button {
+                    
+                } label: {
+                    AuthenticateButton(text: "Sign Up")
+                        .padding()
+                }
+
+                Spacer()
+                
+                Button {
+                    mode.wrappedValue.dismiss()
+                } label: {
+                    HStack {
+                        Text("Already have a account?")
+                            .font(.system(size: 14))
+                        Text("Sign In")
+                            .font(.system(size: 14, weight: .semibold))
+                    } // HStack
+                    .foregroundColor(.white)
+                }
+                .padding(.bottom, 16)
+            } // VStack
+            .padding(.top, 20)
+        } // ZStack
     }
 }
 
---
+++ AllDone/View/Utils/TextFields/UserTextField.swift
@@ -0,0 +1,31 @@
+//
+//  UserTextField.swift
+//  AllDone
+//
+//  Created by Shishir_Mac on 5/11/22.
+//
+
+import SwiftUI
+
+struct UserTextField: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    
+    var body: some View {
+        CustomTextField(text: $text, placeholder: Text(placeholder), imageName: "person")
+            .padding()
+            .background(Color(.init(white: 1, alpha: 0.15)))
+            .cornerRadius(10)
+            .foregroundColor(.white)
+    }
+}
+
+struct UserTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            BackgroundGradientView()
+            UserTextField(text: .constant(""), placeholder: "USer")
+        }
+    }
+}
